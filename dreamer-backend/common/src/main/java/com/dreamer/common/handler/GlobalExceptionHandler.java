a0244a840f6b1eaa1d6692cc603b35c91a2178b6
Ghostars <2185866695@qq.com>
2026-05-16 19:54:16 +0800

    dreamer2.0更新

--- dreamer-backend/common/src/main/java/com/dreamer/common/handler/GlobalExceptionHandler.java
+++ dreamer-backend/common/src/main/java/com/dreamer/common/handler/GlobalExceptionHandler.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static com.dreamer.common.message.SystemMessage.SYSTEM_ERROR;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public SaResult handleException(Exception e) {
+        log.error("系统异常: {}",e.getMessage());
         return SaResult.error(SYSTEM_ERROR);
     }
 }
